cbef1fbb273220bd78d87896aa96b791d4f65ddf
flavien-hugs <geekactux@gmail.com>
2022-07-11 14:22:02 +0000

    edit forms secret key

--- config.py
+++ config.py
@@ -11,6 +11,9 @@ else:
    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, 'core.db')
 
-
 EMAIL_ADDRESS = "support@unsta.net"
 PHONE_NUMBER = "(225) 01 5157 1396"
+
+# https://flask-wtf.readthedocs.io/en/1.0.x/form/#secure-form
+
+WTF_CSRF_SECRET_KEY = os.environ.get('SECRET_KEY')
