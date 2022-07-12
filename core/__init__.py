b6a23346c4ea16f32c02d2aa5e165296c8528568
flavien-hugs <geekactux@gmail.com>
2022-07-12 17:51:44 +0000

    add custom views

--- core/__init__.py
+++ core/__init__.py
@@ -1,16 +1,34 @@
+"""
+Initialize app
+"""
+
 import logging as lg
 
 from flask import Flask
+
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
-from core import routes, models
+login_manager = LoginManager(app)
+login_manager.login_view = 'auth.loginPage'
+
+from core import models
+from .auth import auth as auth_blueprint
+from .main import main as main_blueprint
 
 
 db.init_app(app)
+
+# blueprint for auth routes in our app
+
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(main_blueprint)
 
 @app.cli.command('init_db')
 def init_db():
