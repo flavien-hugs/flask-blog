4a18786e156d515f7b47d1fa8dbf9eef4eff4713
flavien-hugs <geekactux@gmail.com>
2022-07-12 12:18:57 +0000

    initialized database

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,31 @@
+import logging as lg
+
+from core import models
 from core.views import app
+
+user = models.User(
+    username="Flavien HUGS",
+    email="flavienhugs@pm.me",
+    password="password"
+)
+
+post = models.Post(
+    user_id=1,
+    title="Post one",
+    content="First post content one !"
+)
+
+def init_database():
+    models.db.create_all()
+    models.db.session.add(user)
+    models.db.session.add(post)
+    models.db.session.commit()
+    lg.warning('Database initialized !')
+
+
+models.db.init_app(app)
+
+
+@app.cli.command('init_db')
+def init_db():
+    init_database()
