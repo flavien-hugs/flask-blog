a5bed61b066a6814eb72100b349616efdbcf56d4
flavien-hugs <geekactux@gmail.com>
2022-07-17 19:29:51 +0000

    configuration ckeditor

--- core/__init__.py
+++ core/__init__.py
@@ -9,11 +9,13 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+ckeditor = CKEditor(app)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'auth.loginPage'
