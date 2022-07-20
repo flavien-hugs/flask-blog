2f1489dffab0dbe3a6c4525b89f52bfddfb89b1d
flavien-hugs <geekactux@gmail.com>
2022-07-20 03:58:07 +0000

    improve configuration

--- config.py
+++ config.py
@@ -11,30 +11,49 @@ BASE_DIR = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(BASE_DIR, ".flaskeenv"))
 
 
-SECRET_KEY = environ.get('SECRET_KEY')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_ECHO = False
+class Config:
 
-if environ.get('DATABASE_URL'):
-   SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
-else:
-   SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(BASE_DIR, 'site.db')
+    SECRET_KEY = environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+    EMAIL_ADDRESS = "support@unsta.net"
+    PHONE_NUMBER = "(225) 01 5157 1396"
+    FLASKY_ADMIN = "unsta@pm.me"
+    WTF_CSRF_SECRET_KEY = environ.get('SECRET_KEY')
+    MAIL_POST = environ.get('MAIL_SERVER')
+    MAIL_SERVER = environ.get('MAIL_SERVER')
+    MAIL_USE_TLS = environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = environ.get('MAIL_USERNAME')
+    MAIL_SUBJECT_PREFIX = '[Unsta Inc]'
+    MAIL_SENDER = 'Unsta Admin <noreply@unsta.com>'
+    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    RESET_TOKEN_MINUTES = environ.get('RESET_TOKEN_MINUTES')
 
-
-EMAIL_ADDRESS = "support@unsta.net"
-PHONE_NUMBER = "(225) 01 5157 1396"
-
-# https://flask-wtf.readthedocs.io/en/1.0.x/form/#secure-form
-
-WTF_CSRF_SECRET_KEY = environ.get('SECRET_KEY')
-
-# Configuration MAIL SERVER
-
-MAIL_POST = environ.get('MAIL_SERVER')
-MAIL_SERVER = environ.get('MAIL_SERVER')
-MAIL_USE_TLS = environ.get('MAIL_USE_TLS')
-MAIL_USERNAME = environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    @staticmethod
+    def init_app(app):
+        pass
 
 
-RESET_TOKEN_MINUTES = environ.get('RESET_TOKEN_MINUTES')
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL") \
+        or 'sqlite:///' + path.join(BASE_DIR, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = environ.get('TEST_DATABASE_URL') \
+        or 'sqlite://'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') \
+        or 'sqlite:///' + path.join(BASE_DIR, 'data.sqlite')
+
+
+config = {
+    'testing': TestingConfig,
+    'default': DevelopmentConfig,
+    'production': ProductionConfig,
+    'development': DevelopmentConfig,
+}
